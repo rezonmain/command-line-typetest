e171f73bb383e6dd4e991669ef0cbdb4b904153d
rezonmain <adiazgomez101@gmail.com>
2022-06-21 17:43:25 -0700

    fixed header overflow

--- src/components/layout/stats/Stats.js
+++ src/components/layout/stats/Stats.js
@@ -4,28 +4,29 @@ export default function Stats(props) {
 	return (
 		<section className='stats-section'>
 			<Header title='Stats' />
-			<ul className='stats'>
-				<li>
-					Time: <b>{'1hr 30min'}</b>
-				</li>
-				<li>
-					WPM: <b>{'28.3'}</b>
-				</li>
-				<li>
-					Mistakes: <b>{'12'}</b>
-				</li>
-				<li>
-					Accuracy: <b>{'80'}</b>
-				</li>
-				<li>
-					Score: <b>{'234'}</b>
-				</li>
-			</ul>
-			<hr></hr>
-			<button className='button'>Clear console</button>
-			<hr></hr>
-			{/* prettier-ignore */}
-			<pre className='ascii-logo'>
+			<div className='stats-container'>
+				<ul className='stats'>
+					<li>
+						Time: <b>{'1hr 30min'}</b>
+					</li>
+					<li>
+						WPM: <b>{'28.3'}</b>
+					</li>
+					<li>
+						Mistakes: <b>{'12'}</b>
+					</li>
+					<li>
+						Accuracy: <b>{'80'}</b>
+					</li>
+					<li>
+						Score: <b>{'234'}</b>
+					</li>
+				</ul>
+				<hr></hr>
+				<button className='button'>Clear console</button>
+				<hr></hr>
+				{/* prettier-ignore */}
+				<pre className='ascii-logo'>
       {`            (/////,                   
               ////////////              
                  //////////////         
@@ -42,8 +43,9 @@ export default function Stats(props) {
    |C|O|M|M|A|N|D| |T|Y|P|E| |T|E|S|T|
    +-+-+-+-+-+-+-+ +-+-+-+-+ +-+-+-+-+`}
 			</pre>
-			<hr></hr>
-			<small> &copy; 2022 made by rezonmain</small>
+				<hr></hr>
+				<small> &copy; 2022 made by rezonmain</small>
+			</div>
 		</section>
 	);
 }
--- src/components/layout/terminal/Terminal.js
+++ src/components/layout/terminal/Terminal.js
@@ -4,6 +4,7 @@ export default function Terminal() {
 	return (
 		<section className='terminal-section'>
 			<Header title={'/users/rez'} />
+			<div className='terminal-container'></div>
 		</section>
 	);
 }
